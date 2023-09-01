b533d68620d519162e74b8f37bde9bbc45da7adb
johnny9 <985648+johnny9@users.noreply.github.com>
2023-08-31 22:26:06 -0400

    hosttest: Add unit tests for linux target

--- components/bm1397/include/bm1366.h
+++ components/bm1397/include/bm1366.h
@@ -1,26 +1,25 @@
 #ifndef BM1366_H_
 #define BM1366_H_
 
+#include "common.h"
 #include "driver/gpio.h"
 #include "mining.h"
-#include "common.h"
 
 #define CRC5_MASK 0x1F
 
-static const u_int64_t BM1366_FREQUENCY = CONFIG_ASIC_FREQUENCY;
+static const u_int64_t BM1366_FREQUENCY = 425;
 static const u_int64_t BM1366_CORE_COUNT = 672;
-static const u_int64_t BM1366_HASHRATE_S = BM1366_FREQUENCY * BM1366_CORE_COUNT * 1000000;
+static const u_int64_t BM1366_HASHRATE_S =
+    BM1366_FREQUENCY * BM1366_CORE_COUNT * 1000000;
 // 2^32
 // static const u_int64_t NONCE_SPACE = 4294967296;
 static const double BM1366_FULLSCAN_MS = 2140;
 
-typedef struct
-{
+typedef struct {
   float frequency;
 } bm1366Module;
 
-typedef struct __attribute__((__packed__))
-{
+typedef struct __attribute__((__packed__)) {
   uint8_t job_id;
   uint8_t num_midstates;
   uint8_t starting_nonce[4];
--- components/bm1397/include/bm1397.h
+++ components/bm1397/include/bm1397.h
@@ -1,38 +1,36 @@
 #ifndef BM1397_H_
 #define BM1397_H_
 
+#include "common.h"
 #include "driver/gpio.h"
 #include "mining.h"
-#include "common.h"
 
 #define CRC5_MASK 0x1F
 
-static const u_int64_t ASIC_FREQUENCY = CONFIG_ASIC_FREQUENCY;
+static const u_int64_t ASIC_FREQUENCY = 425;
 static const u_int64_t BM1397_CORE_COUNT = 672;
-static const u_int64_t BM1397_HASHRATE_S = ASIC_FREQUENCY * BM1397_CORE_COUNT * 1000000;
+static const u_int64_t BM1397_HASHRATE_S =
+    ASIC_FREQUENCY * BM1397_CORE_COUNT * 1000000;
 // 2^32
 static const u_int64_t NONCE_SPACE = 4294967296;
-static const double BM1397_FULLSCAN_MS = ((double)NONCE_SPACE / (double)BM1397_HASHRATE_S) * 1000;
+static const double BM1397_FULLSCAN_MS =
+    ((double)NONCE_SPACE / (double)BM1397_HASHRATE_S) * 1000;
 
-typedef struct
-{
+typedef struct {
   float frequency;
 } bm1397Module;
 
-typedef enum
-{
+typedef enum {
   JOB_PACKET = 0,
   CMD_PACKET = 1,
 } packet_type_t;
 
-typedef enum
-{
+typedef enum {
   JOB_RESP = 0,
   CMD_RESP = 1,
 } response_type_t;
 
-typedef struct __attribute__((__packed__))
-{
+typedef struct __attribute__((__packed__)) {
   uint8_t job_id;
   uint8_t num_midstates;
   uint8_t starting_nonce[4];

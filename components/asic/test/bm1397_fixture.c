29a1761af228201ea1964394ffe01589a56ab3e2
johnny9 <johnny9dev@pm.me>
2026-09-12 00:31:37 +0000

    test: reject inconsistent BM1397 job slots

--- components/asic/test/bm1397_fixture.c
+++ components/asic/test/bm1397_fixture.c
@@ -89,6 +89,13 @@ void bm1397_fixture_install_job(uint8_t job_id, bm_job *job)
     valid_jobs[job_id] = 1;
 }
 
+void bm1397_fixture_mark_job_valid(uint8_t job_id)
+{
+    TEST_ASSERT_TRUE(job_id < JOB_SLOTS);
+    TEST_ASSERT_NULL(active_jobs[job_id]);
+    valid_jobs[job_id] = 1;
+}
+
 void bm1397_fixture_queue_response(
     const uint8_t response[BM1397_FIXTURE_RESPONSE_SIZE])
 {
